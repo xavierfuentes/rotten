47b2f07f6c2d705e0f5341a600703b475a2cc9e9
Xavier Fuentes <mr.xavierfuentes@gmail.com>
2018-03-28 09:58:24 +0100

    destructure window on the loop

--- packages/client/src/store/enhancers/loop/index.js
+++ packages/client/src/store/enhancers/loop/index.js
@@ -1,6 +1,7 @@
 import { startLoop, tickLoop } from './actions';
 
 export function createGameLoop() {
+  const { requestAnimationFrame, performance } = window;
   return createStore => (reducer, initialState, enhancer) => {
     const store = createStore(reducer, initialState, enhancer);
     const { dispatch, subscribe } = store;
@@ -9,14 +10,14 @@ export function createGameLoop() {
     let fps;
 
     subscribe(function() {
-      window.requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
         const now = performance.now();
         while (times.length > 0 && times[0] <= now - 1000) {
           times.shift();
         }
         times.push(now);
         fps = times.length;
-        dispatch(tickLoop({ fps }))
+        dispatch(tickLoop({ fps }));
       });
     });
 
