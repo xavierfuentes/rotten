97d57f236c904cd7bc27edccd6680b6f435eb41d
Xavi <xavifuefer@gmail.com>
2018-06-07 13:32:32 +0100

    update actor

--- packages/client/src/modules/actor/components/Actor.js
+++ packages/client/src/modules/actor/components/Actor.js
@@ -1,13 +1,16 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import ACTOR_CONSTANTS from '../constants';
 
 class Actor extends React.Component {
-  static propTypes = {};
+  static propTypes = {
+    spawn: PropTypes.func.isRequired
+  };
 
   componentDidMount() {
     this.props.spawn({
+      // these characteristics should come from the character selector state.
       gender: ACTOR_CONSTANTS.GENDER_FEMALE
     });
   }
